3fc1137c29cd7c646c5d85fb920c331ad379e894
greatbritain249 <greatbritain249@gmail.com>
2017-09-11 22:58:44 -0400

    The The files Hello4110.class & hello4110.java were added

---
+++ hello4110-a.java
@@ -0,0 +1,8 @@
+public class hello4110 {
+
+    public static void main(String[] args) {
+        // Prints "This is a Software Engineering course" to the terminal window.
+        System.out.println("This is a Software Engineering course");
+    }
+
+}
